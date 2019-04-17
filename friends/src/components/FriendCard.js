23c12b482770d0918770d6a5d21d868167ed7326
lrod <rleslie1015@gmail.com>
2019-04-17 12:38:47 -0700

    finished todays assignment

---
+++ friends/src/components/FriendCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function FriendCard(props) {
+    return (<div>
+        {props.friend.name}
+    </div>)
+}
+export default FriendCard;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import FriendCard from "./FriendCard";
+import axios from "axios";
+
+
+class FriendsList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            friends: []
+        };
+    }
+    componentDidMount() {
+        axios.get("http://localhost:5000/friends")
+        .then(res => {
+            this.setState({ friends: res.data });
+        })
+        .catch(err => console.log(err));
+    }
+    render() {
+        return(
+            <div>{this.state.friends.map(friend => <FriendCard friend={friend} />)}</div>
+        )
+    }
+
+        
+}
+ 
+export default FriendsList;
